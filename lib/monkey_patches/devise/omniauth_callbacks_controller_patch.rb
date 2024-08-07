8df954c65b366a5f70b09cd3c827180b4bb557ab
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-08-07 17:42:54 +0900

    refactor: review whether the monkey patch is still necessary (#84)

--- lib/monkey_patches/devise/omniauth_callbacks_controller_patch.rb
+++ lib/monkey_patches/devise/omniauth_callbacks_controller_patch.rb
@@ -3,8 +3,8 @@
 
 require "devise/version"
 require "devise_token_auth/version"
-raise "Consider removing this patch." unless Devise::VERSION == "4.9.2"
-raise "Consider removing this patch." unless DeviseTokenAuth::VERSION == "1.2.2"
+raise "Consider removing this patch." unless Devise::VERSION == "4.9.4"
+raise "Consider removing this patch." unless DeviseTokenAuth::VERSION == "1.2.3"
 
 Rails.application.config.to_prepare do
   Devise::OmniauthCallbacksController.class_eval do
