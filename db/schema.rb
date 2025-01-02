233d0be54f616a500a455cf5f9bad9776afba442
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-02 19:30:36 +0900

    feat: add search functionality for users (#204)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_075134) do
+ActiveRecord::Schema[7.2].define(version: 2023_10_17_075134) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_ja_0900_as_cs", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
