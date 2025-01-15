0834134d4aa1232f66183e34505a75155c2b0761
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-15 20:02:08 +0900

    chore: change seed.rb to create user data (#216)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,31 @@
 #   movies = Movie.create([{ name: "Star Wars" },
 #                          { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+
+# Delete all users.
+User.destroy_all
+# Create a user with a fixed password.
+User.create!(
+  name: "テスト ユーザー",
+  email: "text@example.com",
+  password: "sample",
+  password_confirmation: "sample",
+  is_private: false,
+  bio: "テスト用のユーザー",
+  confirmed_at: Time.current
+)
+# Create 20 users.
+20.times do
+  password = Faker::Alphanumeric.alphanumeric(number: 10)
+
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password,
+    is_private: Faker::Boolean.boolean,
+    bio: Faker::Lorem.paragraph(sentence_count: 10),
+    confirmed_at: Time.current
+  )
+end
