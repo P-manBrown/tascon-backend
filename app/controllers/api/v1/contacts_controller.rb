2551f1c861533a0c5d53ffc3f6076be9a1088c4e
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-18 23:46:48 +0900

    feat: add destroy action for contacts (#223)

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -11,6 +11,12 @@ module Api
         end
       end
 
+      def destroy
+        contact = current_api_v1_user.contacts.find(params[:id])
+        contact.destroy
+        head :no_content
+      end
+
       private
         def contact_params
           params.require(:contact).permit(:contact_user_id)
