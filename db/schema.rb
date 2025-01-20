b1787d19b78ac6eed75c7270a65cd214c91ac897
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-20 13:19:32 +0900

    refactor: remove constraints from users bio column
    Update the users table to allow null values and remove the default 
    for the bio column. This change enables users to have an empty bio 
    without enforcing a default value.
    Update the schema version to reflect the migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_19_101340) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_20_040935) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_ja_0900_as_cs", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -69,7 +69,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_19_101340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_private", default: true, null: false
-    t.string "bio", default: "", null: false
+    t.string "bio"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
