09a22ab5e8886d75dd2f54f0044a97dd4cb71350
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-25 23:53:30 +0900

    build: remove typelizer gem (#237)

--- app/resources/error_resource.rb
+++ app/resources/error_resource.rb
@@ -2,6 +2,4 @@ class ErrorResource < ApplicationResource
   root_key :error, :errors
 
   attributes :attribute, :full_message
-
-  typelize attribute: :string, full_message: :string
 end
--- config/initializers/typelizer.rb
+++
@@ -1,3 +0,0 @@
-Typelizer.configure do |config|
-  config.output_dir = Rails.root.join("tmp/typelizer")
-end
