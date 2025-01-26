d190ef4dd5d5a2f59bae88d125ee866f5631d622
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-26 23:16:29 +0900

    feat: include the current user's contact info in the user's details (#238)

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -2,4 +2,8 @@ class UserResource < ApplicationResource
   root_key :user, :users
 
   attributes :id, :name, :bio, :avatar_url
+
+  attribute :current_user_contact, if: proc { !params[:current_user_contact].nil? } do
+    ContactResource.new(params[:current_user_contact], within: :user).to_h
+  end
 end
