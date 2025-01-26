e804dd5c422fda73709f4b346ad81d65b360ec8e
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-26 23:06:18 +0900

    feat: include the current user's contact info in the user's details
    Edit the `show` action in `UsersController` to set the user instance variable and include the current user's contact information in the response. Introduce a private method to retrieve the current user's contact for the specified user. Update the `UserResource` to conditionally include the current user contact in the serialized response.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,9 @@ module Api
   module V1
     class UsersController < ApplicationController
       def show
-        user = User.find(params[:id])
+        @user = User.find(params[:id])
 
-        render json: UserResource.new(user), status: :ok
+        render json: UserResource.new(@user, params: { current_user_contact: }), status: :ok
       end
 
       def search
@@ -16,6 +16,11 @@ module Api
           render json: UserResource.new(user), status: :ok
         end
       end
+
+      private
+        def current_user_contact
+          current_api_v1_user.contacts.find_by(contact_user_id: @user.id)
+        end
     end
   end
 end
