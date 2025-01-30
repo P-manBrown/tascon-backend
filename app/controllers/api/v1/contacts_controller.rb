dc14329aa132c77e9d9ca2b6f5a58e0264acd020
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-01-30 22:36:23 +0900

    feat: add update contact functionality (#248)

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,25 +1,46 @@
 module Api
   module V1
     class ContactsController < ApplicationController
-      def create
-        contact = current_api_v1_user.contacts.build(contact_params)
+      before_action :set_contact, only: %i[update destroy]
 
-        if contact.save
-          render json: ContactResource.new(contact), status: :created, location: api_v1_user_url(contact.contact_user)
+      def create
+        @contact = current_api_v1_user.contacts.build(create_contact_params)
+
+        if @contact.save
+          render json: ContactResource.new(@contact), status: :created, location: api_v1_user_url(@contact.contact_user)
         else
-          render json: ErrorResource.new(contact.errors), status: :unprocessable_entity
+          render_validation_error
+        end
+      end
+
+      def update
+        if @contact.update(update_contact_params)
+          render json: ContactResource.new(@contact), status: :ok
+        else
+          render_validation_error
         end
       end
 
       def destroy
-        contact = current_api_v1_user.contacts.find(params[:id])
-        contact.destroy
+        @contact.destroy
         head :no_content
       end
 
       private
-        def contact_params
+        def create_contact_params
           params.expect(contact: %i[contact_user_id display_name note])
+        end
+
+        def update_contact_params
+          params.expect(contact: %i[display_name note])
+        end
+
+        def set_contact
+          @contact = current_api_v1_user.contacts.find(params[:id])
+        end
+
+        def render_validation_error
+          render json: ErrorResource.new(@contact.errors), status: :unprocessable_entity
         end
     end
   end
