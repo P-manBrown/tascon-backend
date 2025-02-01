881dd4c45f16b39c56c236e2f7bb793203fada8c
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-02-01 19:49:26 +0900

    feat: add contacts note length validation
    Update the note validation to allow a maximum length of 1000 characters.  This change prevents notes that are too long from being saved.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,7 @@ class Contact < ApplicationRecord
 
   validates :contact_user_id, uniqueness: { scope: :user_id }
   validates :display_name, presence: true, allow_nil: true, length: { maximum: 255 }
-  validates :note, presence: true, allow_nil: true
+  validates :note, presence: true, allow_nil: true, length: { maximum: 1000 }
   validate :not_self_contact, on: :create
   validate :contact_user_must_be_public, on: :create
 
