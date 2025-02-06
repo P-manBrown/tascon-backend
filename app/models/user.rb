a524be1892e2c758c16ac262838b830c1ddfdce2
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-02-06 23:59:59 +0900

    feat!: add exclusion validation for bio on update (#258)

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
   validates :avatar, content_type: { in: %w[image/jpeg image/png] }, size: { less_than_or_equal_to: 2.megabytes },
                      processable_file: true, mime_type_and_extension_consistency: true
-  validates :bio, length: { maximum: 250 }, presence: true, allow_nil: true
+  validates :bio, length: { maximum: 250 }
+  validates :bio, exclusion: [nil], on: :update
 
   def avatar=(value)
     if value.respond_to?(:original_filename) && value.original_filename
