502f365dd1a3991fc9167778afe1a64fc9d0bc38
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-02-07 23:35:40 +0900

    feat!: update validations for contact model
    Removed presence validation for display_name and note, allowing empty and nil characters and disallowing nil only on change.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,10 @@ class Contact < ApplicationRecord
   belongs_to :contact_user, class_name: "User"
 
   validates :contact_user_id, uniqueness: { scope: :user_id }
-  validates :display_name, presence: true, allow_nil: true, length: { maximum: 255 }
-  validates :note, presence: true, allow_nil: true, length: { maximum: 1000 }
+  validates :display_name, length: { maximum: 255 }
+  validates :display_name, exclusion: [nil], if: :display_name_changed?
+  validates :note, length: { maximum: 1000 }
+  validates :note, exclusion: [nil], if: :note_changed?
   validate :not_self_contact, on: :create
   validate :contact_user_must_be_public, on: :create
 
