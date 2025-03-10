3f7656eaeabe98f1d76c1cf144c14cb46c89f5e9
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-03-10 16:30:35 +0900

    feat!: improve token validation response
    Refactors the `render_validate_token_success` method to use the
    `AccountResource` serializer, which provides a more consistent and
    structured response for the token validation endpoint. This change
    simplifies the response and removes the need for manual merging of
    the avatar URL.

--- app/controllers/api/v1/auth/token_validations_controller.rb
+++ app/controllers/api/v1/auth/token_validations_controller.rb
@@ -4,11 +4,7 @@ module Api
       class TokenValidationsController < DeviseTokenAuth::TokenValidationsController
         private
           def render_validate_token_success
-            token_validation_response = @resource.token_validation_response.merge(avatar_url: @resource.avatar_url)
-            render json: {
-              success: true,
-              data: resource_data(resource_json: token_validation_response)
-            }
+            render json: AccountResource.new(@resource), status: :ok
           end
       end
     end
