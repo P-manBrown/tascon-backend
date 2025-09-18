ea2884875ae793438132fcf842c8198223fef281
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-09-18 10:58:11 +0900

    feat: add validation to prevent blocking contact users (#438)

--- app/models/block.rb
+++ app/models/block.rb
@@ -3,12 +3,18 @@ class Block < ApplicationRecord
   belongs_to :blocked, class_name: "User"
 
   validates :blocked_id, uniqueness: { scope: :blocker_id }
-  validate :cannot_block_self
+  validate :cannot_block_self, :cannot_block_contact, on: :create
 
   private
     def cannot_block_self
       return unless blocker_id == blocked_id
 
       errors.add(:blocked, :cannot_block_self, message: "に自分自身は指定できません。")
+    end
+
+    def cannot_block_contact
+      return unless blocker.contacts.exists?(contact_user_id: blocked_id)
+
+      errors.add(:blocked, :cannot_block_contact, message: "は登録されているためブロックできません。")
     end
 end
